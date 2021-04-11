ad4f12586c7afaaa42465ba391fc67fa3bec3649
Jean-Julien-Vanrossomme <82309159+Jean-Julien-Vanrossomme@users.noreply.github.com>
2021-04-11 11:22:48 +0200

    Create projet.php

---
+++ projet.php
@@ -0,0 +1,20 @@
+<?php
+	// PARAMETRES
+	$titre = "Projet web";
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<title><?= $titre ?></title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<?php
+        require('config.php');
+
+
+
+		?>
+	</body>
+</html>
