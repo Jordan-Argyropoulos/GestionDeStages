9283d4a44e8e4de64313c10be03a3e6c8b49dab5
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-04-17 18:09:17 +0200

    modif

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_EMAIL', 'root');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'registration');
  
 // Connexion à la base de données MySQL 
-$conn = mysqli_connect(DB_SERVER, DB_EMAIL, DB_PASSWORD, DB_NAME);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Vérifier la connexion
 if($conn === false){
