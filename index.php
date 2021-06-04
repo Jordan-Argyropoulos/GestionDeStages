29adc66c7f16e70a1113ea5fd38ddd9dac4db127
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-06-04 21:44:58 +0200

    04-06-2021 modif vous etes nouveau ici

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
       <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail">
       <input type="password" class="box-input" name="password" placeholder="Mot de passe">
       <input type="submit" value="connexion" name="valider" class="box-button">
+      <p class="box-register">Vous êtes nouveau ici? <a href="frontregister.php">S'inscrire</a></p>
    </form>
-   <p class="box-register">Vous êtes nouveau ici? <a href="frontregister.php">S'inscrire</a></p>
+
 </body>
 </html>
