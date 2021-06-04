59828498114c8237f9438e67dd7aa79514abc970
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-06-04 22:00:37 +0200

    04-06-2021 modif titre dans le formulaire

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
   <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-   <h1 class="box-title">Connexion</h1>
+   
    <form class="box" action="./php/login.php" method="post" name="login">
+      <h1 class="box-title">Connexion</h1>
       <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail">
       <input type="password" class="box-input" name="password" placeholder="Mot de passe">
       <input type="submit" value="connexion" name="valider" class="box-button">
