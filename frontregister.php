99f1fce01c6f26568e2e6fefa158f47105c46f31
Jordan-Argyropoulos <argyropoulosjordan@gmail.com>
2021-06-04 21:13:59 +0200

    insert-register-040621

--- frontregister.php
+++ frontregister.php
@@ -14,8 +14,9 @@
    <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail" required />
    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
    <input type="password" class="box-input" name="password_confirm" placeholder="Confirmation du mot de passe" required />
-   <input type="submit" name="valider" value="S'inscrire" class="box-button" />
-   <p class="box-register">Déjà inscrit? <a href="./php/login.php">Connectez-vous ici</a></p>
+   <input type="number" class="box-input" name="stage" placeholder="Stage oui=1 non=0"/>
+   <input type="submit" name="valider" class="box-button" />
+   <p class="box-register">Déjà inscrit? <a href="index.php">Connectez-vous ici</a></p>
 </form>
 
 
--- php/register.php
+++ php/register.php
@@ -1,24 +1,31 @@
 <?php
    session_start();
    include("config.php");
-   $nom=$_POST["nom_etudiant"];
-   $prenom=$_POST["prenom_etudiant"];
-   $email=$_POST["email_etudiant"];
-   $pass=$_POST["password"];
-   $repass=$_POST["password_confirm"];
-   $erreur="";
-   var_dump($_POST);
+   //var_dump($_POST);
 
-   if(isset($_POST['valider'])){
-      $sel=$pdo->prepare("SELECT id_etudiant from etudiants where email_etudiant=? limit 1");
+   if(isset($_POST["nom_etudiant"]) && ($_POST["prenom_etudiant"]) && ($_POST["email_etudiant"]) && ($_POST["password"]) && ($_POST["password_confirm"])){
+      $nom=$_POST["nom_etudiant"];
+      $prenom=$_POST["prenom_etudiant"];
+      $email=$_POST["email_etudiant"];
+      $pass=$_POST["password"];
+      $repass=$_POST["password_confirm"];
+      $stage=$_POST["stage"];
+   }
+   
+   $erreur="";
+   
+   if(!empty($_POST["email_etudiant"]) && ($_POST["password"])){
+      $sel=$pdo->prepare("SELECT id_etudiant from etudiants where email_etudiant=? ");
       $sel->execute(array($email));
       $tab=$sel->fetchAll();
+      
+      //print_r($tab);
       if(count($tab)>0)
          $erreur="email existe déjà!";
       else{
-         $ins=$pdo->prepare("INSERT into etudiants(nom_etudiant,prenom_etudiant,email_etudiant,password,password_confirm) values(:nom_etudiant,:prenom_etudiant,:email_etudiant,:password,:password_confirm)");
-         if($ins->execute(array($nom,$prenom,$email,$pass,$repass)))
-            header("location:login.php");
+         $ins=$pdo->prepare("INSERT into etudiants(nom_etudiant,prenom_etudiant,email_etudiant,password,password_confirm,stage) values(?,?,?,?,?,?)");
+         if($ins->execute(array($nom,$prenom,$email,$pass,$repass,$stage)))
+            header("location:/php/login.php");
       }   
    }
 ?>
