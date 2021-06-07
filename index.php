6aed058872677881a0b58b714ed04d5e12e036dd
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-06-07 21:06:50 +0200

    07/06/21 modif

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
   <script src="js/script.js" type="text/javascript"></script>
 </head>
 <body>
-   
-   <form class="box" action="./php/login.php" method="post" name="login">
-      <h1 class="box-title">Connexion</h1>
-      <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail">
-      <input type="password" class="box-input" name="password" placeholder="Mot de passe">
-      <input type="submit" value="connexion" name="valider" class="box-button">
-      <p class="box-register">Vous êtes nouveau ici? <a href="frontregister.php">S'inscrire</a></p>
-   </form>
-
+<div id="page">
+      <div id="scr_login">  
+         <form class="box" action="./php/login.php" method="post" name="login">
+            <h1 class="box-title">Connexion</h1>
+            <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail" id="inp_login">
+            <input type="password" class="box-input" name="password" placeholder="Mot de passe" id="inp_passwd">
+            <input type="submit" value="connexion" name="valider" class="box-button" id="inp_enter">
+            <p class="box-register">Vous êtes nouveau ici? <a href="frontregister.php">S'inscrire</a></p>
+         </form>
+      </div>
+</div>
 </body>
 </html>
