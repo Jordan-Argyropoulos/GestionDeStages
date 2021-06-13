a8cce99611b262dc8d172e6c60842f842dcd447b
Jean-Julien-Vanrossomme <jj.vanrossomme@outlook.com>
2021-06-13 11:13:25 +0200

    modif 13/06/21

--- frontregister.php
+++ frontregister.php
@@ -8,16 +8,16 @@
 	</head>
 	<body>
 
-<form class="box" action="./php/register.php" method="post" id="inscription">
+<form class="box" action="./php/register.php" method="post" >
 	<h1 class="box-logo box-title"><a>Inscription</a></h1>
    <h1 class="box-title">S'inscrire</h1>
 	<input type="text" class="box-input" name="nom_etudiant" placeholder="Nom" required  />
    <input type="text" class="box-input" name="prenom_etudiant" placeholder="Prénom" required  />
-   <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail" required />
-   <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
+   <input type="text" class="box-input" name="email_etudiant" placeholder="Adresse mail" required id="email" />
+   <input type="password" class="box-input" name="password" placeholder="Mot de passe" required id="password"/>
    <input type="password" class="box-input" name="password_confirm" placeholder="Confirmation du mot de passe" required  />
    <input type="number" class="box-input" name="stage" placeholder="Stage oui=1 non=0" required />
-   <input type="submit" name="valider" class="box-button"  />
+   <input type="submit" name="valider" class="box-button" onclick="validate()" />
    <p class="box-register">Déjà inscrit? <a href="index.php">Connectez-vous ici</a></p>
 </form>
 		
